dc0b75667e9d8778a740596e5ac0ab6e768a9083
Yss207 <suryawanshiyash04@gmail.com>
2025-03-01 10:37:25 +0530

    Edits

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './Routes/user.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 // Use built-in express.json() middleware
@@ -11,10 +13,11 @@ app.use(express.json());
 app.use('/api', userRouter);
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://suryawanshiyash07:DdrQiiednOEhWLwF@cluster0.wrgw7.mongodb.net/', {
-    dbName: 'MERN_Recipe'
-}).then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log('MongoDB Connection Error:', err));
+mongoose.connect(process.env.MONGO_URI).then(
+    () => console.log("MongoDB connected successfully..")
+).catch(
+    (err) => console.log(err)
+)
 
 const port = 3000;
 
